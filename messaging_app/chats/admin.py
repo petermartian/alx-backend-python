3993f2173796b1cda6d776a4b3bddd24409510cf
petermartian <petmartian@gmail.com>
2025-08-18 22:49:29 +0100

    lint(chats): satisfy E302 by adding blank lines in admin

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Conversation, Message
+
+
 @admin.register(Conversation)
 class ConversationAdmin(admin.ModelAdmin):
-    list_display = ("title", "created_at", "updated_at")
-    search_fields = ("title",)
-    filter_horizontal = ("participants",)
-    ordering = ("-created_at",)
+    pass
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
